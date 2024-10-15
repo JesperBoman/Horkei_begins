22dc270d4f1d3abd58604cf72757894533e58fb8
JesperBoman <74507057+JesperBoman@users.noreply.github.com>
2024-10-15 17:19:51 +0200

    Update 02_FisherJunction_pairwise_sharing.sh

--- Fisher_junction_sharing/02_FisherJunction_pairwise_sharing.sh
+++ Fisher_junction_sharing/02_FisherJunction_pairwise_sharing.sh
@@ -23,7 +23,7 @@ do
 for s2 in "${arr2[@]}"
 do
 
-
+#Count the pairwise overlap
 bedtools intersect -a $dir/$s1.$chromosome.fj.bed -b $dir/$s2.$chromosome.fj.bed -wo | awk -v s1=$s1 -v s2=$s2 -v chr=$chromosome 'BEGIN{sum=0}{sum+=$7} END{print chr "\t" s1 "\t" s2 "\t" sum "\t" NR}' >> fisher.junction.pairwise.results
 
 
@@ -39,3 +39,6 @@ p=$(($p+1))
 done
 
 done < "scaffolds.list.Arx.auto"
+
+#Normalize on a scale from 0 (no sharing) to 1 (complete sharing)
+awk 'NR==FNR{if($2 == $3){self[$1,$2]=$5}} NR!=FNR{NormOverlap=2*($5/(self[$1,$2]+self[$1,$3])); print $0 "\t" self[$1,$2] "\t" self[$1,$3] "\t" NormOverlap }' fisher.junction.pairwise.results fisher.junction.pairwise.results > fisher.junction.pairwise.results.norm
