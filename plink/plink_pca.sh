42662dba5f68acf561fd2701d67e45ebe3d6002b
JesperBoman <74507057+JesperBoman@users.noreply.github.com>
2024-10-15 13:44:12 +0200

    Create plink_pca.sh

---
+++ plink/plink_pca.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -l
+
+ml bioinfo-tools plink2/2.00-alpha-3.7-20221024
+
+
+prefix="ArxRef.preBQSR.auto.LD.50kb.1.0.5"
+
+plink2 --threads 2 --bfile $prefix --pca --allow-extra-chr --bad-freqs --out $prefix
