008e69450e4aa8703518dd8d9743ea69fa44f01a
JesperBoman <74507057+JesperBoman@users.noreply.github.com>
2024-10-15 13:10:15 +0200

    Create variant_filtering_forPixy.sh

---
+++ 03_Variant_calling/variant_filtering_forPixy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+
+
+#Load modules
+module load bioinfo-tools vcftools/0.1.16 samtools/1.19
+
+#gzvcf="Arx.preBQSR.allsites.dedup.g.vcf"
+gzvcf="Arx.preBQSR.allsites.dedup.g.Z_ploidyHapF.vcf"
+
+vcftools --gzvcf $gzvcf.gz \
+--remove-indels \
+--max-missing 0.8 \
+--min-meanDP 10 \
+--max-meanDP 200 \
+--recode --stdout | bgzip -@ 2 > $gzvcf.pixy_filt.gz
+
+tabix -p vcf $gzvcf.pixy_filt.gz 
