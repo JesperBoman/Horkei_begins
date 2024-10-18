9537ea2b6d9fed89cb32279d6c95034ac3fada35
JesperBoman <74507057+JesperBoman@users.noreply.github.com>
2024-10-18 11:06:51 +0200

    Create ahmm_panel_step3.sh

---
+++ ancestry_hmm_inference/ahmm_panel_step3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+
+
+while IFS= read -r sample
+do
+
+let i++
+let j=i+1
+
+awk 'FNR==NR && FNR>1 && $3==2 {split($5, REF, ":"); split($6, ALT, ":"); sREF[$1,$2]=REF[2]; sALT[$1,$2]=ALT[2]} FNR!=NR{print $0 "\t" sREF[$1,$2] "\t" sALT[$1,$2] }' Individual_allele_counts/$sample.frq.count tmp.$i.txt > tmp.$j.txt
+
+done < "samples.list.Horkei"
+
+mv tmp.$j.txt step3.txt
